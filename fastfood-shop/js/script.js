fb77c6e5642d600c2a0603e313bd376bb9ebc229
Koldun17 <86883749+Koldun17@users.noreply.github.com>
2024-04-21 22:38:20 +0300

    Add files via upload

---
+++ fastfood-shop/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){ 
+  $('.slider-promo').slick({
+    prevArrow: '.slick-prev',
+    nextArrow: '.slick-next',
+    dots:true,
+    appendDots:'.slide-dots',
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    customPaging:function(){return ''}
+  });
+
+ 
+
+
+
+  
+
+  });
